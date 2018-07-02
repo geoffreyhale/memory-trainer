9bde9e8472402ec232b4c2351b25761f991ea389
geoffreyhale <geoffreyhale@gmail.com>
2018-07-01 22:51:24 -0700

    Game class

--- index.js
+++ index.js
@@ -19,18 +19,16 @@ class Puzzle {
         let _numberStringEl = null;
         let _userInputEl = null;
 
-        this.start = function() {
+        const startNumber = function() {
             _numberString = randomNumberString(_digitCount);
 
             _numberStringEl = document.createElement('span');
             _numberStringEl.setAttribute('id', 'random-number');
             _numberStringEl.innerHTML = _numberString;
             document.body.appendChild(_numberStringEl);
-            
-            setTimeout(this.timeToGuess, _secondsVisible*1000); //@todo add visible timer
         };
 
-        this.timeToGuess = function() {
+        const timeToGuess = function() {
             document.body.removeChild(_numberStringEl);
 
             _userInputEl = document.createElement('input');
@@ -40,9 +38,14 @@ class Puzzle {
             _userInputEl.focus();
         };
 
+        this.start = function() {
+            startNumber();
+            setTimeout(timeToGuess, _secondsVisible*1000); //@todo add visible timer
+        };
+
         this.solved = function() {
             if (_userInputEl && _userInputEl.value === _numberString) {
-                console.log('correct');
+                console.log('correct - ' + _numberString);
                 document.body.removeChild(_userInputEl);
                 return true;
             }
@@ -51,22 +54,44 @@ class Puzzle {
     }
 }
 
-function startGame() {
-    let digitCount = 1;
-    let secondsVisible = 3;
+class Game {
+    constructor(gameEl) {
+        let _digitCount = 1;
+        let _secondsVisible = 3;
+        let _puzzle = null;
 
-    let puzzle = new Puzzle(digitCount, secondsVisible);
-    puzzle.start();
+        const _gameEl = gameEl;
+        let _digitCountEl = null;
+        
+        const _createDigitCountEl = function() {
+            _digitCountEl = document.createElement('h2');
+            _gameEl.appendChild(_digitCountEl);
+        };
 
-    document.addEventListener('keyup', function() {
-        if (puzzle.solved()) {
-            digitCount++;
-            puzzle = new Puzzle(digitCount, secondsVisible);
-            puzzle.start();
-        }
-    });
+        const _startNextPuzzle = function() {
+            _digitCountEl.innerText = "Level " + _digitCount;
+            _puzzle = new Puzzle(_digitCount, _secondsVisible);
+            _puzzle.start();
+        };
+
+        this.start = function() {
+            _createDigitCountEl();
+            _startNextPuzzle();
+
+            document.addEventListener('keyup', function() {
+                if (_puzzle.solved()) {
+                    _digitCount++;
+                    _startNextPuzzle();
+                }
+            });
+        };
+    }
 }
 
 window.onload = function() {
-    startGame();
+    const gameEl = document.getElementById('game');
+    if (gameEl) {
+        const game = new Game(gameEl);
+        game.start();
+    }
 };
