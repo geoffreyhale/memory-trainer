127266a707b7e34d7b08d64a950899ed80074e35
geoffreyhale <geoffreyhale@gmail.com>
2018-07-01 21:59:28 -0700

    Puzzle class

--- index.js
+++ index.js
@@ -1,50 +1,72 @@
-let digits = 1;
-let showSeconds = 3;
-
 function randomDigit() {
     return Math.floor(Math.random() * 10);
 }
 
-function randomNumberString(digits) {
+function randomNumberString(digitCount) {
     const randomNumberStringArray = [];
-    for (let i = 0; i < digits; i++) {
+    for (let i = 0; i < digitCount; i++) {
         randomNumberStringArray.push(randomDigit().toString());
     }
     return randomNumberStringArray.join('');
 }
 
-function startPuzzle(digits, showSeconds) {
-    const rns = randomNumberString(digits);
-    const randomNumberEl = document.createElement('span');
-    randomNumberEl.setAttribute('id', 'random-number');
-    randomNumberEl.innerHTML = rns;
-    document.body.appendChild(randomNumberEl);
-    setTimeout(timeToGuess.bind(null, randomNumberEl, rns), showSeconds*1000); //@todo add visible timer
-}
+class Puzzle {
+    constructor(digitCount, secondsVisible) {
+        const _digitCount = digitCount;
+        const _secondsVisible = secondsVisible;
 
-function timeToGuess(randomNumberEl, solutionString) {
-    document.body.removeChild(randomNumberEl);
-    const input = document.createElement('input');
-    input.setAttribute('id', 'guess');
-    document.addEventListener('keyup', checkAnswer.bind(null, solutionString, input));
-    document.body.appendChild(input);
-    input.focus();
-}
+        let _numberString = '';
+        let _numberStringEl = null;
+        let _userInputEl = null;
 
-function checkAnswer(solutionString, input) {
-    const guess = document.getElementById('guess').value;
-    if (guess === solutionString) {
-        correctAnswer(input);
+        this.start = function() {
+            _numberString = randomNumberString(_digitCount);
+
+            _numberStringEl = document.createElement('span');
+            _numberStringEl.setAttribute('id', 'random-number');
+            _numberStringEl.innerHTML = _numberString;
+            document.body.appendChild(_numberStringEl);
+            
+            setTimeout(this.timeToGuess, _secondsVisible*1000); //@todo add visible timer
+        };
+
+        this.timeToGuess = function() {
+            document.body.removeChild(_numberStringEl);
+
+            _userInputEl = document.createElement('input');
+            _userInputEl.setAttribute('id', 'guess');
+            document.body.appendChild(_userInputEl);
+
+            _userInputEl.focus();
+        };
+
+        this.solved = function() {
+            if (_userInputEl && _userInputEl.value === _numberString) {
+                console.log('correct');
+                document.body.removeChild(_userInputEl);
+                return true;
+            }
+            return false;
+        };
     }
 }
 
-function correctAnswer(input) {
-    console.log('correct');
-    document.body.removeChild(input);
-    digits++;
-    startPuzzle(digits, showSeconds);
+function startGame() {
+    let digitCount = 1;
+    let secondsVisible = 3;
+
+    let puzzle = new Puzzle(digitCount, secondsVisible);
+    puzzle.start();
+
+    document.addEventListener('keyup', function() {
+        if (puzzle.solved()) {
+            digitCount++;
+            puzzle = new Puzzle(digitCount, secondsVisible);
+            puzzle.start();
+        }
+    });
 }
 
 window.onload = function() {
-    startPuzzle(digits, showSeconds);
+    startGame();
 };
